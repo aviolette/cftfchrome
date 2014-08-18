8d84dfd6c91a8d3b91e0651898852c0d015b2251
aviolette <aviolette@gmail.com>
2014-08-17 20:59:55 -0500

    Encapsulated javascript code

---
+++ options.js
@@ -0,0 +1,14 @@
+
+function loadSettings() {
+  chrome.storage.sync.get({ searchRadius : 0.25}, function(items) {
+    $("#radius").val(items.searchRadius.toString());
+  });
+}
+
+$(document).ready(loadSettings);
+
+$("#save").click(function() {
+  var radius = parseFloat($("#radius").val());
+  chrome.storage.sync.set({ searchRadius : radius}, function() {
+  });
+});
