308ebd1f5e9e36865dcee4295764b0af849e609c
Andrew Violette <aviolette@gmail.com>
2015-04-15 21:12:28 -0500

    Added logo48x48.png to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm cftfchrome.zip
-zip cftfchrome.zip background.js icon.png jquery-2.1.1.min.js manifest.json options.html options.js popup.html popup.js logo128x128.png
+zip cftfchrome.zip background.js icon.png jquery-2.1.1.min.js manifest.json options.html options.js popup.html popup.js logo128x128.png logo48x48.png
 zip -r cftfchrome.zip bootstrap
